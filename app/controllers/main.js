caabf59d8cbde9503ad7a8e4fbd4a5b35f2726f2
bobbyziom <bobbyziom@gmail.com>
2015-03-26 12:46:19 +0100

    init

---
+++ app/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = require('../app');
+
+function main(req, res) {
+  res.send('Server is up');
+}
+
+function test(req, res) {
+	res.send('hej heeej');
+}
+
+// Routes
+app.get('/', main);
+
+app.get('/hej', test);
