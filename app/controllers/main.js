ed3caee87cfd9484810561f79da8eb49abe08a0c
bobbyziom <bobbyziom@gmail.com>
2015-03-27 13:32:12 +0100

    specific song request

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -4,7 +4,8 @@ var main = require('../app');
 
 // Route handlers
 function status(req, res) {
-  	res.send('Server is up!');
+	console.log("Server is up!");
+  	res.status(200).send({ msg: 'Server is up!' });
 }
 
 // Routes
--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -6,13 +6,14 @@ var ObjectID = require('mongodb').ObjectID;
 
 
 function songs(req, res) {
+	console.log("Finding all songs");
     db.collection('songs').find().toArray(function (err, docs) {
       res.send(docs);
     });
 }
 
 function insert(req, res) {
-	console.log(req.body);
+	console.log("Creating new song: " + req.body);
   	db.collection('songs').insert(req.body, function(err, records) {
 		if (err) throw err;
 		res.sendStatus(200);
@@ -20,7 +21,7 @@ function insert(req, res) {
 }
 
 function erase(req, res) {
-	console.log(req.params.id);
+	console.log("Erasing song with id: " + req.params.id);
 	db.collection('songs').remove({ _id: ObjectID(req.params.id) }, function(err, result) {
 		if(err) { throw err; }
 		res.sendStatus(200);
@@ -28,14 +29,25 @@ function erase(req, res) {
 }
 
 function update(req, res) {
-	console.log(req.params.id);
+	console.log("Updating song with id: " + req.params.id);
+	console.log("New data: " + req.body);
 	db.collection('songs').update({ _id: ObjectID(req.params.id)}, req.body, function(err, result) {
 		if(err) { throw err; }
 		res.sendStatus(200);
 	});
 }
 
+function find(req, res) {
+	console.log("Finding song with id: " + req.params.id);
+	db.collection('songs').findOne({ _id: ObjectID(req.params.id)}, function(err, result) {
+		if(err) { throw err; }
+		res.status(200).json(result);
+	});
+}
+
 // Routes
+
+main.app.get('/songs/:id', find);
 
 main.app.get('/songs', songs);
 
